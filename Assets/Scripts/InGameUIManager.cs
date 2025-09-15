3439a2256f846977855f1319dcdfb08fcc7871f4
Choi Jeongho <jeonghoe22@gmail.com>
2025-09-16 00:15:02 +0900

    클리어 조건 버그 수정

--- Assets/Scripts/InGameUIManager.cs
+++ Assets/Scripts/InGameUIManager.cs
@@ -159,11 +159,8 @@ private void timeCalculate()
             return;
         }
 
-        // 목표 점수에 도달했으면, 바로 통과
-        if(currentGoalValue.score == 0)
-            ClearUI(true);
-
-        // 목표 점수에 도달하지 못하면, 계속 플레이
+        // 목표에 도달해서 바로 통과
+        ClearUI(true);
     }
 
     // 클리어 UI 출력

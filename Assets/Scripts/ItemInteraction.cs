124a378dd23a1620f17c5ae96a0a6648cd7a927a
Choi Jeongho <jeonghoe22@gmail.com>
2025-09-15 11:52:14 +0900

    아이템 재생성시, 다시 상호작용이 가능하도록 버그 수정

--- Assets/Scripts/ItemInteraction.cs
+++ Assets/Scripts/ItemInteraction.cs
@@ -18,16 +18,20 @@ public class ItemInteraction : MonoBehaviour
         FloatingItem();
     }
 
+    // 아이템 활성화
     public void SetStartPos(Vector3 pos)
     {
         startPos = pos;
+        isOnce = true;
     }
 
+    // 아이템 회전
     private void RotateItem()
     {
         transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
     }
 
+    // 아이템 둥둥 떠다니는 효과
     private void FloatingItem()
     {
         transform.position = startPos + Vector3.up * Mathf.Sin(Time.time * floatFrequency) * floatAmplitude;
